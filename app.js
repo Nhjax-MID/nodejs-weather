4c7545e85babb8cdb9197576509157c9200e4c23
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-22 13:29:41 -0400

    testing new output

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var parser = require('weather-alerts-parser');
 var request = require('request');
 var sensor = require('node-dht-sensor');
-var axios = require('axios');
 
 sensor.read(11, 4, function(err, temperature, humidity) {
     if (!err) {
@@ -15,7 +14,9 @@ sensor.read(11, 4, function(err, temperature, humidity) {
 
 axios.get('http://alerts.weather.gov/cap/us.php?x=0')
 .then((response) => {
-  console.log(response.data);
+  parser.stream();
+  es.stringify();
+  process.stdout;
 })
 .catch(err => {
   console.log(err.response);
