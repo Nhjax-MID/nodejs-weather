7b0226b6c129efa450a9fded7cc71fa14cd33a1f
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-22 11:57:59 -0400

    INIT NWS Alerts

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var es = require('event-stream');
+var fs = require('fs');
+var parser = require('weather-alerts-parser');
+var request = require('request');
 var sensor = require('node-dht-sensor');
 
 sensor.read(11, 4, function(err, temperature, humidity) {
@@ -7,3 +11,8 @@ sensor.read(11, 4, function(err, temperature, humidity) {
         );
     }
 });
+
+request.get('http://alerts.weather.gov/cap/us.php?x=1')
+  .pipe(parser.stream())
+  .pipe(es.stringify())
+  .pipe(process.stdout);
