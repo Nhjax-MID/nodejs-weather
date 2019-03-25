3c1f39d6146c71ca75469ddb6a0397350cf2b44a
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-25 12:06:52 -0400

    remove from callbak

--- app.js
+++ app.js
@@ -28,22 +28,23 @@ sensor.read(11, 4, function(err, temperature, humidity) {
         );
         this.temp = (temperature.toFixed(1));
         this.hum = (humidity.toFixed(1));
-        client.on('connect', function () {
-          console.log('connect', this.temp);
-          client.subscribe('test', function (err) {
-            if (!err) {
-              console.log('test', this.temp);
-              client.publish('test', this.temp)
-            }
-          })
-        })
-
-        client.on('message', function (topic, message) {
-          // message is Buffer
-          console.log('message', this.temp);
-          console.log(message.toString())
-          client.end()
-        })
 
     }
 });
+
+client.on('connect', function () {
+  console.log('connect', this.temp);
+  client.subscribe('test', function (err) {
+    if (!err) {
+      console.log('test', this.temp);
+      client.publish('test', this.temp)
+    }
+  })
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log('connect', this.temp);
+  console.log(message.toString())
+  client.end()
+})
