95f37c94d7c5557e5a673ab1cf3c6af359611794
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-25 13:55:18 -0400

    test

--- app.js
+++ app.js
@@ -2,75 +2,49 @@ var mqtt = require('mqtt');
 var sensor = require('node-dht-sensor');
 var temp;
 var hum;
-var MQTT_TOPIC = "test";
-var MQTT_ADDR = "mqtt://test.mosquitto.org";
-var MQTT_PORT = 1883;
-var client  = mqtt.connect(MQTT_ADDR,{
-  clientId: 'bgtestnodejs',
-  protocolId: 'MQIsdp',
-  protocolVersion: 3,
-  connectTimeout:1000,
-  debug:true
-});
+var mqtt = require('mqtt');
 
-function startProgram(){
-  sensor.read(11, 4, function(err, temperature, humidity) {
-      if (!err) {
-          console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-              'humidity: ' + humidity.toFixed(1) + '%'
-          );
-          temp = (temperature.toFixed(1));
-          hum = (humidity.toFixed(1));
-
-          console.log(temp);
-          console.log(hum);
-          console.log("Entering client on");
-          clientOn();
-
-      }
-      else {
-        console.log(err);
-      }
-  })
-};
-
-
-// clientOnError = () => {
-//   client.on('error', function(){
-//       console.log("ERROR")
-//       client.end()
-//   });
-// };
-
-function clientOnPublisher(){
-  client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log("Inside of message");
-    console.log('connect', topic, message);
-    console.log(message.toString());
-    client.end();
-  });
-};
-
-function clientOn(){
-  console.log("Inside of clientOn");
-  client.on('connect', function () {
-    console.log("Inside of client on");
-    console.log('connect', temp);
-  client.subscribe(MQTT_TOPIC, function (err) {
-    if (!err) {
-      client.publish(MQTT_TOPIC, temp)
-    }
-    else {
-      console.log("Theres an error inside of subscribe");
-    }
-  })
-  })
-};
-
-startProgram();
+var MQTT_TOPIC          = "hello";
+var MQTT_ADDR           = "mqtt://test.mosquitto.org";
+var MQTT_PORT           = 1883;
 
 /* This is not working as expected */
 //var client = mqtt.connect({host: MQTT_ADDR, port:MQTT_PORT},{clientId: 'bgtestnodejs'});
 
 /* This works... */
+var client  = mqtt.connect(MQTT_ADDR,{clientId: 'bgtestnodejs', protocolId: 'MQIsdp', protocolVersion: 3, connectTimeout:1000, debug:true});
+
+sensor.read(11, 4, function(err, temperature, humidity) {
+    if (!err) {
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%'
+        );
+        temp = (temperature.toFixed(1));
+        hum = (humidity.toFixed(1));
+
+        console.log(temp);
+        console.log(hum);
+        console.log("Entering client on");
+        clientOn();
+
+    }
+    else {
+      console.log(err);
+    }
+});
+
+client.on('connect', function () {
+    client.subscribe(MQTT_TOPIC);
+    client.publish(MQTT_TOPIC, 'Hello mqtt');
+});
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+    client.end();
+});
+
+client.on('error', function(){
+    console.log("ERROR")
+    client.end()
+});
