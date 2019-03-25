eeb4c3867c38122f4c687cb8154c854c2bebfcda
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-25 14:29:45 -0400

    move function

--- app.js
+++ app.js
@@ -12,7 +12,26 @@ var MQTT_PORT           = 1883;
 //var client = mqtt.connect({host: MQTT_ADDR, port:MQTT_PORT},{clientId: 'bgtestnodejs'});
 
 /* This works... */
-var client  = mqtt.connect(MQTT_ADDR,{clientId: 'bgtestnodejs', protocolId: 'MQIsdp', protocolVersion: 3, connectTimeout:1000, debug:true});
+
+function callMQTT(temp, hum){
+
+  var client  = mqtt.connect(MQTT_ADDR,{clientId: 'bgtestnodejs', protocolId: 'MQIsdp', protocolVersion: 3, connectTimeout:1000, debug:true});
+
+
+  client.on('connect', function () {
+    client.subscribe('presence', function (err) {
+      if (!err) {
+        client.publish('presence', temp);
+      }
+    })
+  })
+
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    client.end()
+  })
+};
 
 //function run(){
   sensor.read(11, 4, function(err, temperature, humidity) {
@@ -36,21 +55,6 @@ var client  = mqtt.connect(MQTT_ADDR,{clientId: 'bgtestnodejs', protocolId: 'MQI
   });
 //};
 
-function callMQTT(temp, hum){
-  client.on('connect', function () {
-    client.subscribe('presence', function (err) {
-      if (!err) {
-        client.publish('presence', temp);
-      }
-    })
-  })
-
-  client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
-    client.end()
-  })
-}
 
 
 // client.on('connect', function () {
