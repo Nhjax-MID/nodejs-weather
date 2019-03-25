2d10be3dcd4a860b05d8573ff9418dc5557bf0a0
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-25 10:53:38 -0400

    MQTT testing

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
 var sensor = require('node-dht-sensor');
+
+client.on('connect', function () {
 
 sensor.read(11, 4, function(err, temperature, humidity) {
     if (!err) {
+      client.publish('test', temperature.toFixed(1))
         console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
             'humidity: ' + humidity.toFixed(1) + '%'
         );
     }
+})
 });
