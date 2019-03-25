b147205daecc2f962c4543fcf66238e121cfc7e1
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-25 07:16:07 -0400

    complete rewrite from last commit, testing new moduals

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
-var es = require('event-stream');
-var fs = require('fs');
-var parser = require('weather-alerts-parser');
 var request = require('request');
-var sensor = require('node-dht-sensor');
-var axios = require('axios');
 
-sensor.read(11, 4, function(err, temperature, humidity) {
+var headers = {
+  'User-Agent': 'NMRTC WEATHER APP',
+  'Content-Type' : 'application/ld+json',
+  'Accept' : 'application/vnd.noaa.dwml+json;version=1'
+}
+request({
+  headers: {
+     'User-Agent': 'NMRTC WEATHER APP',
+     'Content-Type' : 'application/json',
+     'Accept' : 'application/json'
+  },
+   url: 'https://alerts.weather.gov/cap/wwaatmget.php?x=FLC031&y=1',
+   method: 'POST'
+ }, function (err, res, body) {
+   console.log(body);
+ });
+
+
+
+
+/*    .then(metars => {
+      console.log(JSON.stringify(metars, null, 2))
+    })
+
+*/
+
+
+
+
+
+
+
+/*var req = require('request');
+
+req.post({
+   url: 'https://alerts.weather.gov/cap/wwaatmget.php?x=FLC031&y=1',
+   headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36',
+      'Content-Type' : 'application/ld+json'
+   },
+   method: 'GET'
+  },
+
+  function (e, r, body) {
+      console.log(body);
+  });
+*/
+
+
+/*sensor.read(11, 4, function(err, temperature, humidity) {
     if (!err) {
         console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
             'humidity: ' + humidity.toFixed(1) + '%'
         );
     }
 });
+*/
 
 /*axios.get('http://alerts.weather.gov/cap/us.php?x=0')
 .then((response) => {
@@ -23,7 +68,3 @@ sensor.read(11, 4, function(err, temperature, humidity) {
   console.log(err.response);
 });
 */
-request.get('http://alerts.weather.gov/cap/us.php?x=1')
-  .pipe(parser.stream())
-  .pipe(es.stringify())
-  .pipe(process.stdout);
