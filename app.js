9a6cdec244393c8379904109d246024a83f8833b
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-25 10:56:59 -0400

    Testing MQTT

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ client.on('connect', function () {
 sensor.read(11, 4, function(err, temperature, humidity) {
     if (!err) {
       client.publish('test', temperature.toFixed(1))
+      client.end()
         console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
             'humidity: ' + humidity.toFixed(1) + '%'
         );
