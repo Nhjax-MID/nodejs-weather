456e7424b70ba2f7318945b09a9e4216836b0a53
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-25 13:41:19 -0400

    reconfigured mqtt

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect("mqtt://mqtt.swifitch.cz");
+var client = require('mqtt');
 var sensor = require('node-dht-sensor');
 var temp;
 var hum;
+var MQTT_TOPIC = "test";
+var MQTT_ADDR = "mqtt://test.mosquitto.org";
+var MQTT_PORT = 1883;
+var client  = mqtt.connect(MQTT_ADDR,{
+  clientId: 'bgtestnodejs',
+  protocolId: 'MQIsdp',
+  protocolVersion: 3,
+  connectTimeout:1000,
+  debug:true
+});
+
+client.on('error', function(){
+    console.log("ERROR")
+    client.end()
+});
 
 function publisher(){
   client.on('message', function (topic, message) {
@@ -13,14 +28,14 @@ function publisher(){
     client.end();
   });
 };
+
 clientOn = () => {
   client.on('connect', function () {
     console.log("Inside of client on");
     console.log('connect', temp);
-  client.subscribe('presence', function (err) {
+  client.subscribe(MQTT_TOPIC, function (err) {
     if (!err) {
-      client.publish('test', temp)
-      client.end();
+      client.publish(MQTT_TOPIC, temp)
     }
     else {
       console.log("Theres an error inside of subscribe");
@@ -29,6 +44,24 @@ clientOn = () => {
   });
 });
 
+sensor.read(11, 4, function(err, temperature, humidity) {
+    if (!err) {
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%'
+        );
+        temp = (temperature.toFixed(1));
+        hum = (humidity.toFixed(1));
+
+        console.log(temp);
+        console.log(hum);
+        console.log("Entering client on");
+        clientOn();
+
+    }
+    else {
+      console.log(err);
+    }
+});
 // client.on('message', function (topic, message) {
 //   // message is Buffer
 //   console.log(message.toString());
@@ -53,21 +86,7 @@ clientOn = () => {
 //   });
 };
 
-sensor.read(11, 4, function(err, temperature, humidity) {
-    if (!err) {
-        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%'
-        );
-        temp = (temperature.toFixed(1));
-        hum = (humidity.toFixed(1));
+/* This is not working as expected */
+//var client = mqtt.connect({host: MQTT_ADDR, port:MQTT_PORT},{clientId: 'bgtestnodejs'});
 
-        console.log(temp);
-        console.log(hum);
-        console.log("Entering client on");
-        clientOn();
-
-    }
-    else {
-      console.log(err);
-    }
-});
+/* This works... */
