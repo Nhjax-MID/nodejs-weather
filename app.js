5fa7e0aa1f6e14cf0f45f8eb654c1962cb0c0ddf
Kevin nichols <kevinnichols1321@icloud.com>
2019-03-26 11:49:21 -0400

    testing

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var MQTT_TOPIC          = "test";//sets topic
 var MQTT_ADDR           = "mqtt://76.106.248.100"; //address of subscriber
 var MQTT_PORT           = 1883; //common MQTT port
 
-function callMQTT(temp, hum){ //wrapped MQTT message handler in function callMQTT
+function callMQTT(temp, hum, results){ //wrapped MQTT message handler in function callMQTT
 
   var client  = mqtt.connect(MQTT_ADDR,{
     clientId: 'bgtestnodejs',
@@ -21,28 +21,23 @@ function callMQTT(temp, hum){ //wrapped MQTT message handler in function callMQT
   client.on('connect', function () { //MQTT message handler "Publisher"
     client.subscribe(MQTT_TOPIC, function (err) {
       if (!err) {
-          console.log("Entering Message Publisher");
         let obj = {temp:temp,hum:hum,results}; //oject is assigned value
         buf = Buffer.from(JSON.stringify(obj)); //buffer is dumped into a JSON object using obj
-        console.log("Memory dumped");
         client.publish(MQTT_TOPIC, buf); //message is pulished to subscriber
-          console.log("Message Sent to server");
       }
     })
   })
 
   client.on('message', function (topic, message) { //message is echoed on publisher terminal
     // message is Buffer
-    console.log("Entering Echo");
     console.log(JSON.parse(message.toString()))
-        console.log("Exiting Echo");
     client.end() //Client is terminated
   })
 
   client.on('error', function(){ //Error handler
-    console.log("Entering error");
-      console.log("DANGER WILL ROBINSON ERROR ERROR MESSAGE HANDLER FAILED DESTROY ROBINSON FAMILY DESTROY JUPITER ONE")
-      console.log("Exiting error");
+    console.log("");
+      console.log("DANGER WILL ROBINSON ERROR ERROR MESSAGE HANDLER FAILED DESTROY ROBINSON FAMILY DESTROY JUPITER ONE");
+      console.log("");
       client.end()
   })
 
@@ -57,22 +52,16 @@ function WX(){
           temp = (temperature.toFixed(1));
           hum = (humidity.toFixed(1));
 
-          console.log("SENSOR READ SUCCESSFUL " + temp);
-          console.log("SENSOR READ SUCCESSFUL " + hum);
-          console.log("Exiting Sensor.read");
-          console.log("Calling callMQTT");
-
           let options = {
             mode: 'json',};
 
           PythonShell.run('script.py', options, function (err, results) {
             if (err) throw err;
-            console.log('finished script');
-            console.log(results);
+            console.log('Results from Python ' + results);
             results = (results);
           });
 
-          callMQTT(temp, hum);
+          callMQTT(temp, hum, results);
 
       }
       else {
